d8e152c1c1a3af0678813eab120aac6daefefc1c
Nicholas Mastracola <nicholas.mastracola@gmail.com>
2017-03-04 03:24:53 -0700

    refactoring enpoints; location is more in line with other endpoints now

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var Category = require('./lib/endpoints/category');
 var Style = require('./lib/endpoints/style');
 var Fermentables = require('./lib/endpoints/fermentables');
 var Location = require('./lib/endpoints/location')
+var Geopoint = require('./lib/endpoints/geopoint')
 var Config  = require('./config');
 
 function BreweryDB(key) {
@@ -16,7 +17,8 @@ function BreweryDB(key) {
     this.style = new Style(this.config);
     this.category = new Category(this.config);
     this.fermentables = new Fermentables(this.config);
-    this.location = new Location(this.config)
+    this.location = new Location(this.config);
+    this.geopoint = new Geopoint(this.config);
 }
 
 module.exports = BreweryDB;
